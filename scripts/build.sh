de8a25c4868b00130f4bda8bb5b72462a52e6a38
Jake Howard <git@theorangeone.net>
2021-05-15 16:38:54 +0100

    Use `build` to build packages
    https://github.com/pypa/build/

--- scripts/build.sh
+++ scripts/build.sh
@@ -6,10 +6,8 @@ export PATH=env/bin:${PATH}
 
 set -x
 
-pip install wheel
-
 rm -rf *.egg-info build/ dist/
 
 python setup.py clean
 
-python setup.py sdist bdist_wheel
+python -m build --sdist --wheel .
